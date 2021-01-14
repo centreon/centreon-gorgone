12dce9d1d0c9f2d298fac5d2cdcec7e092c50f88
garnier-quentin <garnier.quentin@gmail.com>
2021-01-14 16:25:14 +0100

    fix indent

--- gorgone/modules/core/proxy/class.pm
+++ gorgone/modules/core/proxy/class.pm
@@ -448,9 +448,9 @@ sub event_internal {
     my (%options) = @_;
 
     return if (
-            defined($connector->{clients}->{ $options{channel} }) && 
-            ($connector->{clients}->{ $options{channel} }->{com_read_internal} == 0 || $connector->{clients}->{ $options{channel} }->{delete} == 1)
-        );
+        defined($connector->{clients}->{ $options{channel} }) && 
+        ($connector->{clients}->{ $options{channel} }->{com_read_internal} == 0 || $connector->{clients}->{ $options{channel} }->{delete} == 1)
+    );
 
     my $socket = $options{channel} eq 'control' ? $connector->{internal_socket} : $connector->{internal_channels}->{ $options{channel} };
     while (1) {
